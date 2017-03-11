d3cf18e8878331b459d8a26344e6646db1c98921
feitzi <mathias@feitzinger.org>
2017-03-11 09:55:12 +0100

    init project with sample angular2 code

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import './rx-addons';
+import 'hammerjs';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpModule, RequestOptions } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MomentModule } from 'angular2-moment';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule, HttpModule, FlexLayoutModule.forRoot(), ReactiveFormsModule, RouterModule.forRoot(rootRouterConfig), MomentModule
+  ],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  
+}
